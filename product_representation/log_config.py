be62713934e1856c22b3ee876e66c997790d51fe
Uginius <e.slastin@gmail.com>
2022-09-29 14:57:30 +0300

    fix logging mode

--- product_representation/log_config.py
+++ product_representation/log_config.py
@@ -4,8 +4,7 @@ import sys
 root_logger = logging.getLogger()
 root_logger.level = logging.INFO
 log_handler = logging.StreamHandler(stream=sys.stdout)
-file_handler = logging.FileHandler(filename='logs/prre_oz_wb.log', mode='a', encoding='utf8')
-# stdout_formatter = logging.Formatter(fmt='%(levelname)s | %(name)s | %(asctime)s | %(lineno)d | %(message)s')
+file_handler = logging.FileHandler(filename='logs/prre_oz_wb.log', mode='w', encoding='utf8')
 formatter = logging.Formatter(fmt='%(levelname)s | %(lineno)d | %(message)s')
 log_handler.setFormatter(formatter)
 file_handler.setFormatter(formatter)
