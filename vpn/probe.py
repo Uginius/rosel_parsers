f4fb544c2e917306ada90fb83a07c17ceee663ea
Eugene <e.slastin@gmail.com>
2022-10-11 19:16:50 +0300

    proxy probe done

--- vpn/probe.py
+++ vpn/probe.py
@@ -2,15 +2,17 @@ from random import choice
 import requests
 from bs4 import BeautifulSoup
 from config import req_headers
-from vpn.sequre import addr, ips
+from vpn.sequre import login, password, ips
 
 
 def get_location(url):
-    rq = requests.get(url, headers=req_headers, proxies={'https': addr + choice(ips)})
+    proxy_dict = {'https': f'http://{login}:{password}@{choice(ips)}'}
+    rq = requests.get(url, headers=req_headers, proxies=proxy_dict)
     soup = BeautifulSoup(rq.text, 'lxml')
     ip_adr = soup.find('div', id='d_clip_button').text.strip()
     location = soup.find('div', class_='value value-country').text.strip()
     print(f'IP: {ip_adr}, Location: {location}')
 
-    if __name__ == '__main__':
-        get_location('https://2ip.ru/')
+
+if __name__ == '__main__':
+    get_location('https://2ip.ru/')
