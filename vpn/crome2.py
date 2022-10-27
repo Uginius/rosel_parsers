441182e9782a1b4f6dc5a1900988334cd4df3f53
Uginius <e.slastin@gmail.com>
2022-10-27 10:47:18 +0300

    Chrome2 proxy added

--- vpn/crome2.py
+++ vpn/crome2.py
@@ -10,55 +10,58 @@ log = set_logging('browser')
 
 
 class Chrome2:
-    def __init__(self, sandbox=False, timeout=30):
+    def __init__(self, sandbox=False, timeout=20, proxy_passless=None):
         self.options = uc.ChromeOptions()
         sa = selenium_arguments if sandbox is False else selenium_arguments[:-2]
         for sel_arg in sa:
             self.options.add_argument(sel_arg)
-        self.browser = None
+        self.proxy = proxy_passless
+        if proxy_passless:
+            self.options.add_argument(f'--proxy-server={proxy_passless}')
+        self.chrome = None
         self.timeout = timeout
         self.set_browser()
 
     def set_browser(self):
-        self.browser = uc.Chrome(options=self.options, version_main=undetected_chromedriver_ver)
-        self.browser.set_page_load_timeout(self.timeout)
-        self.browser.maximize_window()
+        self.chrome = uc.Chrome(options=self.options, version_main=undetected_chromedriver_ver)
+        self.chrome.set_page_load_timeout(self.timeout)
+        self.chrome.maximize_window()
 
     def get(self, url, wait_time=3, trs=3):
         try:
-            self.browser.get(url)
-        except Exception as ex:
+            self.chrome.get(url)
+        except Exception:
             trs = trs - 1 if trs > 0 else 0
-            log.error(ex)
+            log.error(f'Browser exeption: {url}')
             if trs:
-                log.info(f'*** attempts left: {trs}, try to get {url} again')
+                log.info(f'Attempts left: {trs}, try to get {url} again')
                 self.get(url, wait_time=3, trs=trs)
         time.sleep(wait_time)
-        return self.browser.page_source
+        return self.chrome.page_source
 
     def page_source(self):
-        return self.browser.page_source
+        return self.chrome.page_source
 
     def scroll_down(self, wait_time=0.5):
-        last_height = self.browser.execute_script("return document.body.scrollHeight")
-        self.browser.execute_script(f"window.scrollTo(0, {last_height});")
+        last_height = self.chrome.execute_script("return document.body.scrollHeight")
+        self.chrome.execute_script(f"window.scrollTo(0, {last_height});")
         time.sleep(wait_time)
         while True:
-            self.browser.execute_script(f"window.scrollTo(0, document.body.scrollHeight);")
-            new_height = self.browser.execute_script("return document.body.scrollHeight")
+            self.chrome.execute_script(f"window.scrollTo(0, document.body.scrollHeight);")
+            new_height = self.chrome.execute_script("return document.body.scrollHeight")
             time.sleep(wait_time)
             if new_height == last_height:
                 break
             last_height = new_height
 
     def scroll_up(self, wait_time=0.5):
-        self.browser.execute_script(f"window.scrollTo(0, 0);")
+        self.chrome.execute_script(f"window.scrollTo(0, 0);")
         time.sleep(wait_time)
 
     def close(self):
         try:
-            self.browser.close()
-            self.browser.quit()
+            self.chrome.close()
+            self.chrome.quit()
         except Exception as e:
             log.error(f'Browser error, {e}')
 
